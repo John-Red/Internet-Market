f723cc2810aeebbae0cd573edfa517a9df8fba09
Karim Magomedov <ma9omedov@gmail.com>
2019-07-04 16:57:46 +0300

    Update db-tables.sql
    change ID to user_id in users table, add name to items table, change int to SERIAL for id

--- src/main/resources/db-tables.sql
+++ src/main/resources/db-tables.sql
@@ -1,23 +1,24 @@
 CREATE TABLE users (
-ID int PRIMARY KEY,
+user_id int SERIAL,
 login varchar(25) NOT NULL ,
 password varchar(25) NOT NULL,
 role varchar(25),
-active varchar(25)
+active boolean
 );
 
 CREATE TABLE orders (
-order_id int PRIMARY KEY,
+order_id SERIAL PRIMARY KEY,
 user_id int REFERENCES users (ID)
 );
 
 CREATE TABLE categories (
-category_id int PRIMARY KEY,
+category_id SERIAL PRIMARY KEY,
 name VARCHAR
 );
 
 CREATE TABLE items (
-item_id int PRIMARY KEY,
+item_id SERIAL PRIMARY KEY,
+name VARCHAR(25),
 category_id int REFERENCES categories (category_id),
 price int,
 available int
@@ -28,3 +29,4 @@ item_id int REFERENCES items (item_id),
 order_id int REFERENCES orders (order_id),
 count int
 );
+
