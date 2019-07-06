67a60ab90ab1ab544cf0182f2b446ec4f72dd0b8
John Red <voiceofdust@gmail.com>
2019-07-06 16:19:23 +0300

    saving log in file

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,15 @@
+import org.apache.log4j.Logger;
 import server.ServerApplication;
 
 public class Application {
+
+  private static final Logger LOGGER = Logger.getLogger(Application.class.getSimpleName());
   public static void main(String[] args) {
+    LOGGER.info("Application is started");
     try {
       ServerApplication.INSTANCE.start();
     } catch (Exception e) {
+      LOGGER.error("ServerApplication exeption");
       e.printStackTrace();
     }
   }
--- src/main/java/server/ServerApplication.java
+++ src/main/java/server/ServerApplication.java
@@ -3,12 +3,15 @@ package server;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.log4j.Logger;
+
 import javax.servlet.http.HttpServlet;
 import java.io.File;
 
 /** Creates Server application with tomcat first servlet is on http://localhost:8080/baseServlet */
 public enum ServerApplication {
   INSTANCE;
+  private static final Logger LOGGER = Logger.getLogger(ServerApplication.class.getSimpleName());
 
   private static final String SERVLET_NAME = "Servlet1";
   private static final String URL_PATTERN = "/baseServlet";
@@ -16,6 +19,7 @@ public enum ServerApplication {
 
   public void start() throws LifecycleException {
     configInit();
+    LOGGER.info("ServerApplicatin is started");
 
     Tomcat tomcat = new Tomcat();
     tomcat.setBaseDir(config.getBasedir());
