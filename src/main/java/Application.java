dcc875c1f89788d6fc564a8273911a3024871214
Karim Magomedov <ma9omedov@gmail.com>
2019-07-09 18:33:03 +0300

    delete unused imports in Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,17 @@
-import entities.Items;
-import repositories.ItemRepository;
-import repositories.impl.ItemRepositoryImpl;
 import utils.ServerApplication;
 import lombok.extern.log4j.Log4j;
 
-import java.util.List;
 
 @Log4j
 public class Application {
 
-  public static void main(String[] args) {
-    log.info("Application is started");
-    try {
-      ServerApplication.INSTANCE.start();
-    } catch (Exception e) {
-      log.error(e.getMessage(), e);
-    }
+    public static void main(String[] args) {
+        log.info("Application is started");
+        try {
+            ServerApplication.INSTANCE.start();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
 
-  }
+    }
 }
