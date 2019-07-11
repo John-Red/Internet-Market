31a2b2f2823d97a8f5197e21d57db3ca7870b79f
Karim Magomedov <ma9omedov@gmail.com>
2019-07-11 19:55:10 +0300

    add users repo, admin jsp, admin servlet, admin service

--- src/main/java/repositories/impl/UsersRepositoryImpl.java
+++ src/main/java/repositories/impl/UsersRepositoryImpl.java
@@ -5,9 +5,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.jdbc.core.RowMapper;
+import repositories.UsersRepository;
 import utils.DatabaseConnection;
 
-public enum UsersRepositoryImpl {
+public enum UsersRepositoryImpl implements UsersRepository {
   INSTANCE;
 
   public List<Users> get() {
@@ -16,7 +17,6 @@ public enum UsersRepositoryImpl {
             .getConnection()
             .query(
                 "SELECT * FROM users",
-                new Object[] {},
                 new RowMapper<Users>() {
                   public Users mapRow(ResultSet rs, int rowNum) throws SQLException {
                     return Users.builder()
@@ -28,6 +28,7 @@ public enum UsersRepositoryImpl {
                         .build();
                   }
                 });
+
     return result;
   }
 
@@ -35,15 +36,16 @@ public enum UsersRepositoryImpl {
     DatabaseConnection.INSTANCE
         .getConnection()
         .update(
-            "INSERT INTO users (login, password, role, active) VALUES (?, ?, ?, ?);",
+            "INSERT INTO items (name, category_id, price, available) VALUES (?, ?, ?, ?);",
             login,
             password,
             role,
             active);
   }
 
-  public boolean delete(Integer user_id) {
-    String sql = "DELETE FROM users WHERE user_id = ?";
-    return DatabaseConnection.INSTANCE.getConnection().update(sql, user_id) == 1;
+  public boolean delete(Long id) {
+    String sql = "DELETE FROM items WHERE user_id = ?";
+    return DatabaseConnection.INSTANCE.getConnection().update(sql, id) == 1;
   }
 }
+
