ec9239f1e2463725518ba14c6f6bdeece2ff73a1
yurysukhorukov <30953718+yurysukhorukov@users.noreply.github.com>
2019-07-22 21:27:06 +0300

    solved problem with items availability reduce

--- src/main/java/repositories/impl/ItemsRepositoryImpl.java
+++ src/main/java/repositories/impl/ItemsRepositoryImpl.java
@@ -1,7 +1,6 @@
 package repositories.impl;
 
 import entities.Items;
-import java.net.Inet4Address;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -19,60 +18,67 @@ public enum ItemsRepositoryImpl implements ItemsRepository {
   JdbcTemplate statement = DatabaseConnection.INSTANCE.getStatement();
 
   public List<Items> get() {
-    result = statement.query("SELECT * FROM items", new RowMapper<Items>() {
-      public Items mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return Items.builder()
-            .itemId(rs.getLong("item_id"))
-            .name(rs.getString("name"))
-            .categoryId(rs.getLong("category_id"))
-            .price(rs.getInt("price"))
-            .available(rs.getInt("available"))
-            .image(rs.getString("image") != null
-                ? rs.getString("image") : "default.jpg")
-            .build();
-      }
-    });
+    result =
+        statement.query(
+            "SELECT * FROM items",
+            new RowMapper<Items>() {
+              public Items mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return Items.builder()
+                    .itemId(rs.getLong("item_id"))
+                    .name(rs.getString("name"))
+                    .categoryId(rs.getLong("category_id"))
+                    .price(rs.getInt("price"))
+                    .available(rs.getInt("available"))
+                    .image(rs.getString("image") != null ? rs.getString("image") : "default.jpg")
+                    .build();
+              }
+            });
     return result;
   }
 
   public List<Items> get(Long itemId) {
-    result = statement.query("SELECT * FROM items WHERE item_id = ?", new RowMapper<Items>() {
-          public Items mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return Items.builder()
-                .itemId(rs.getLong("item_id"))
-                .name(rs.getString("name"))
-                .categoryId(rs.getLong("category_id"))
-                .price(rs.getInt("price"))
-                .available(rs.getInt("available"))
-                .image(rs.getString("image") != null ?
-                    rs.getString("image") : "default.jpg")
-                .build();
-          }
-        },
-        itemId);
+    result =
+        statement.query(
+            "SELECT * FROM items WHERE item_id = ?",
+            new RowMapper<Items>() {
+              public Items mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return Items.builder()
+                    .itemId(rs.getLong("item_id"))
+                    .name(rs.getString("name"))
+                    .categoryId(rs.getLong("category_id"))
+                    .price(rs.getInt("price"))
+                    .available(rs.getInt("available"))
+                    .image(rs.getString("image") != null ? rs.getString("image") : "default.jpg")
+                    .build();
+              }
+            },
+            itemId);
     return result;
   }
 
   public List<Items> getByCategory(Long categoryId) {
-    result = statement.query("SELECT * FROM items WHERE category_id = ?", new RowMapper<Items>() {
-          public Items mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return Items.builder()
-                .itemId(rs.getLong("item_id"))
-                .name(rs.getString("name"))
-                .categoryId(rs.getLong("category_id"))
-                .price(rs.getInt("price"))
-                .available(rs.getInt("available"))
-                .image(rs.getString("image") != null ?
-                    rs.getString("image") : "default.jpg")
-                .build();
-          }
-        },
-        categoryId);
+    result =
+        statement.query(
+            "SELECT * FROM items WHERE category_id = ?",
+            new RowMapper<Items>() {
+              public Items mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return Items.builder()
+                    .itemId(rs.getLong("item_id"))
+                    .name(rs.getString("name"))
+                    .categoryId(rs.getLong("category_id"))
+                    .price(rs.getInt("price"))
+                    .available(rs.getInt("available"))
+                    .image(rs.getString("image") != null ? rs.getString("image") : "default.jpg")
+                    .build();
+              }
+            },
+            categoryId);
     return result;
   }
 
   public void insert(String name, long categoryId, int price, int available, String image) {
-    String sql = "INSERT INTO items (name, category_id, price, available, image) VALUES (?, ?, ?, ?,?);";
+    String sql =
+        "INSERT INTO items (name, category_id, price, available, image) VALUES (?, ?, ?, ?,?);";
     statement.update(sql, name, categoryId, price, available, image);
   }
 
@@ -90,7 +96,7 @@ public enum ItemsRepositoryImpl implements ItemsRepository {
     String sql = "SELECT COUNT(*) FROM items WHERE item_id = ?";
     boolean exists = false;
     try {
-      exists = statement.queryForObject(sql, new Object[]{id}, Integer.class) > 0;
+      exists = statement.queryForObject(sql, new Object[] {id}, Integer.class) > 0;
     } catch (Exception e) {
       log.error(e);
     } finally {
@@ -102,7 +108,7 @@ public enum ItemsRepositoryImpl implements ItemsRepository {
     String sql = "SELECT COUNT(*) FROM items WHERE name = ?";
     boolean exists = false;
     try {
-      exists = statement.queryForObject(sql, new Object[]{name}, Integer.class) > 0;
+      exists = statement.queryForObject(sql, new Object[] {name}, Integer.class) > 0;
     } catch (Exception e) {
       log.error(e);
     } finally {
@@ -110,8 +116,8 @@ public enum ItemsRepositoryImpl implements ItemsRepository {
     }
   }
 
-  public void reduceQtyOfAvailableItems (Long itemId, Integer quantity ) {
-    String sql = "UPDATE items SET available = available - ? WHERE item_id = ?";
-    statement.update(sql, quantity, itemId);
+  public void setQtyOfAvailableItems(Integer available, Long itemId) {
+    String sql = "UPDATE items SET available = ? WHERE item_id = ?";
+    statement.update(sql, available, itemId);
   }
 }
--- src/main/java/service/CartService.java
+++ src/main/java/service/CartService.java
@@ -3,12 +3,10 @@ package service;
 import entities.Cart;
 import java.util.List;
 import lombok.extern.log4j.Log4j;
-import repositories.OrdersRepository;
 import repositories.impl.CartRepositoryImpl;
 import repositories.impl.ItemOrderRepositoryImpl;
 import repositories.impl.ItemsRepositoryImpl;
 import repositories.impl.OrdersRepositoryImpl;
-import repositories.impl.UsersRepositoryImpl;
 
 @Log4j
 public enum CartService {
@@ -24,14 +22,6 @@ public enum CartService {
 
   public boolean delete(Long item_order_id) {
     return CartRepositoryImpl.INSTANCE.delete(item_order_id);
-  }
-
-  public Integer getCartQuantity() {
-    if (CartRepositoryImpl.INSTANCE.getSumCartQuantity() == null) {
-      return 0;
-    } else {
-      return CartRepositoryImpl.INSTANCE.getSumCartQuantity();
-    }
   }
 
   public void incrementQuantity(Long item_order_id) {
@@ -52,19 +42,22 @@ public enum CartService {
     }
   }
 
-  public Long getOrderIdForCurrentUser(Long userId){
+  public Long getOrderIdForCurrentUser(Long userId) {
     return OrdersRepositoryImpl.INSTANCE.getOrderIdForCurrentUser(userId);
   }
 
-  public void setOrderToFalse(Long userId){
+  public void setOrderToFalse(Long userId) {
     OrdersRepositoryImpl.INSTANCE.setOrderToFalse(getOrderIdForCurrentUser(userId));
   }
 
-  public void reduceQtyOfAvailableItems (List<Cart> listOfCartItems) {
+  public Integer reduceQtyOfAvailableItems(Long item_id, Integer quantity) {
+    return CartRepositoryImpl.INSTANCE.availablityOfItem(item_id) - quantity;
+  }
+
+  public void setQtyOfAvailableItems(List<Cart> listOfCartItems) {
     for (Cart c : listOfCartItems) {
-      ItemsRepositoryImpl.INSTANCE.reduceQtyOfAvailableItems(c.getItemId(), c.getItemOrdersQuantity());
+      ItemsRepositoryImpl.INSTANCE.setQtyOfAvailableItems(
+          reduceQtyOfAvailableItems(c.getItemId(), c.getItemOrdersQuantity()), c.getItemId());
     }
-
-
   }
 }
