2818e864012a62edf40ebba7e0036a865aa283e3
yurysukhorukov <30953718+yurysukhorukov@users.noreply.github.com>
2019-07-23 13:11:44 +0300

    Added balance to header

--- src/main/java/servlets/AdminServlet.java
+++ src/main/java/servlets/AdminServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j;
 import service.AdminService;
 import service.CartService;
+import service.UsersService;
 
 @Log4j
 public class AdminServlet extends HttpServlet {
@@ -27,6 +28,7 @@ public class AdminServlet extends HttpServlet {
     for (Cart c : list ) {sumCartQuantity += c.getItemOrdersQuantity();
     }
     req.setAttribute("itemsList", listOfItems);
+    req.setAttribute("userBalance", UsersService.INSTANCE.getUserBalance(Cart.currentUserId));
     req.setAttribute("CartQuantity", sumCartQuantity);
     req.getRequestDispatcher("/admin.jsp").forward(req, resp);
   }
